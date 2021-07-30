3529280d5ff5720b2317df9ebbf968d25471d751
gasada <gasada@protonmail.com>
2021-07-31 02:51:07 +0300

    feat: add keyLayout and Create keys in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+// TODO LIST
+// Проверить код на ковычки, привести к однообразию
+// функции, везде юзать стрелочные
+// const с большой
 const KEYBOARD = {
   elements: {
     main: null,
@@ -7,7 +11,7 @@ const KEYBOARD = {
 
   EVENTHANDLERS: {
     oninput: null,
-    oncloce: null
+    onclose: null
   },
 
   PROPERTIES: {
@@ -16,19 +20,45 @@ const KEYBOARD = {
   },
 
   init() {
-    this.elements.main = document.createElement('div');
-    this.elements.keysContainer = document.createElement('div');
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
 
-    this.elements.main.classList.add('keyboard', '1keyboard--hidden');
-    this.elements.keysContainer.classList.add('keyboard__keys');
+    this.elements.main.classList.add("keyboard", "keyboard--hidden");
+    this.elements.keysContainer.classList.add("keyboard__keys");
 
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
   },
 
   _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+      "space"
+    ];
+
+    const createIconHTML = (icon_name) => {
+      return `<i class ="material-icons">${icon_name}</i>`;
+    };
+
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement('button');
+      const insertLineBreak = ["backspace", "p", "enter", "?"].indexOf(key) !== -1;
+    });
+  },
+
+  _triggerEvent(handlerName) {
 
   },
 
-  triggerEvent(handlerName) {
+  _toggleCapsLock() {
+
+  },
+
+  open(initialValue, oninput, onclose) {
 
   }
 };
